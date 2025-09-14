3e9d9c40efebfa6a5348ec30b40a3e1ca35e92ca
Valentin <pamerra@mail.ru>
2025-09-14 21:57:33 +0300

    Добавил кнопку Sign Out на Profile Screen

--- Event Hub/Modules/ProfileScreen/ProfileView.swift
+++ Event Hub/Modules/ProfileScreen/ProfileView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @EnvironmentObject var authManager: AuthenticationManager
+    @Environment(\.colorScheme) private var colorScheme
+    
     var body: some View {
-        Text("This is Profile Screen")
+        VStack {
+            Text("This is Profile Screen")
+            Button {
+                authManager.signOut()
+            } label: {
+                Text("Sign Out")
+                    .font(.headline)
+                    .foregroundColor(colorScheme == .dark ? Color(red: 0.1, green: 0.1, blue: 0.1) : Color.black)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .cornerRadius(8)
+            }
+        }
     }
 }
 
