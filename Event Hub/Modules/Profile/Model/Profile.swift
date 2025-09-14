f33dea7623d7f902d8811e51f9cf268571b490a3
RookCYP <general.ludd.vs@gmail.com>
2025-09-14 20:58:08 +0400

    Create ProfileView (#20)

---
+++ Event Hub/Modules/Profile/Model/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  Event Hub
+//
+//  Created by Aleksandr Zhazhoyan on 09.09.2025.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var description: String
+    var avatarData: Data?
+}
---
+++ Event Hub/Modules/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileViewModel.swift
+//  Event Hub
+//
+//  Created by Aleksandr Zhazhoyan on 09.09.2025.
+//
+
+import Foundation
+import SwiftUI
+
+final class ProfileViewModel: ObservableObject {
+    @Published var profile: Profile
+    private let authManager: AuthenticationManager
+    
+    @Published var isShowingImagePicker = false
+    @Published var isEditingProfile = false
+    
+    @Published var selectedImage: UIImage? {
+        didSet {
+            if let image = selectedImage {
+                profile.avatarData = image.jpegData(compressionQuality: 0.8)
+            }
+        }
+    }
+    
+    init(profile: Profile, authManager: AuthenticationManager) {
+        self.profile = profile
+        self.authManager = authManager
+    }
+    
+    func editProfile() {
+        isEditingProfile = true
+    }
+    
+    func signOut() {
+        authManager.signOut()
+    }
+}
