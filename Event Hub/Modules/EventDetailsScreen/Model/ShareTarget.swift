1f1fdbdb8d620ee8350939f0d26c2d20ee6a6eef
SergeyZakurakin <pricezak@gmail.com>
2025-09-19 21:44:07 -0400

    add model

--- Event Hub/Modules/EventDetailsScreen/Model/ShareTarget.swift
+++ Event Hub/Modules/EventDetailsScreen/Model/ShareTarget.swift
@@ -7,12 +7,48 @@
 
 import SwiftUI
 
-struct ShareTarget: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+// MARK: - MODEL
+
+enum ShareTarget: Identifiable, Hashable {
+    case copyLink
+    case whatsapp
+    case telegram
+    case facebook
+    case messenger
+    case twitter
+    case instagram
+    case iMessage
+
+    var id: String { title }
+    var title: String {
+        switch self {
+        case .copyLink:  return "Copy Link"
+        case .whatsapp:  return "WhatsApp"
+        case .telegram:  return "Telegram"
+        case .facebook:  return "Facebook"
+        case .messenger: return "Messenger"
+        case .twitter:   return "Twitter"
+        case .instagram: return "Instagram"
+        case .iMessage:  return "iMessage"
+        }
+    }
+    
+    var iconAssetName: String {
+        switch self {
+        case .copyLink:  return "copy"
+        case .whatsapp:  return "whatsApp"
+        case .telegram:  return "telegram"
+        case .facebook:  return "facebook"
+        case .messenger: return "messenger"
+        case .twitter:   return "twitter"
+        case .instagram: return "instagram"
+        case .iMessage:  return "message"
+        }
     }
 }
 
-#Preview {
-    ShareTarget()
-}
+
+
+
+
+    
