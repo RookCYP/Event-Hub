f12d11b46835bfcfb5bd14d197211ac564a2eda8
SergeyZakurakin <pricezak@gmail.com>
2025-09-19 21:44:34 -0400

    create cell

--- Event Hub/Modules/EventDetailsScreen/Views/ShareCellView.swift
+++ Event Hub/Modules/EventDetailsScreen/Views/ShareCellView.swift
@@ -7,12 +7,38 @@
 
 import SwiftUI
 
+// MARK: - Ячейка сетки
+
 struct ShareCellView: View {
+    let target: ShareTarget
+    let iconSize: CGFloat
+    let isCopyStyle: Bool
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 6) {
+           
+                Image(target.iconAssetName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: iconSize, height: iconSize)
+                    .clipped()
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+            
+
+            Text(target.title)
+                .font(.footnote)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.primary)
+        }
+        .frame(maxWidth: .infinity)
     }
 }
 
-#Preview {
-    ShareCellView()
+
+#Preview("WhatsApp") {
+    ShareCellView(target: .whatsapp,
+                  iconSize: 40,
+                  isCopyStyle: false)
+        .padding()
+        .background(Color(.systemBackground))
 }
