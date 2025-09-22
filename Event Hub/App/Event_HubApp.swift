b2ec1e6a3f34277fd88c5129f9573d95b0b69698
RookCYP <general.ludd.vs@gmail.com>
2025-09-22 19:38:46 +0300

    fixOnboarding

--- Event Hub/App/Event_HubApp.swift
+++ Event Hub/App/Event_HubApp.swift
@@ -12,14 +12,19 @@ import Firebase
 struct Event_HubApp: App {
     let persistenceController = PersistenceController.shared
     @StateObject private var authManager = AuthenticationManager()
-    
+    @AppStorage("hasSeenOnboarding") private var hasSeenOnboarding: Bool = false
+
     init() {
         FirebaseApp.configure()
     }
     
     var body: some Scene {
         WindowGroup {
-            if authManager.isAuthenticated {
+            if !hasSeenOnboarding {
+                OnboardingView()
+                    .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                    .environmentObject(authManager)
+            } else if authManager.isAuthenticated {
                 MainView()
                     .environment(\.managedObjectContext, persistenceController.container.viewContext)
                     .environmentObject(authManager)
